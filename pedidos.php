4bde8ba9a4d6c9c1f1b4d2f9f9319a2510ed13e5
Sofia Donlucas Bañuelos <sof.db@outlook.com>
2023-08-02 20:46:02 -0600

    CourseCard and CourseInfo component

--- pedidos.php
+++ pedidos.php
@@ -4,52 +4,19 @@ use MailerSend\Helpers\Builder\Personalization;
 use MailerSend\Helpers\Builder\Recipient;
 use MailerSend\Helpers\Builder\EmailParams;
 
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    // Retrieve form data from $_POST superglobal
-    $date = $_POST["date"];
-    $fees = $_POST["fees"];
-    $name = $_POST["name"];
-    $total = $_POST["total"];
-    $subtotal = $_POST["subtotal"];
-    $receipt_id = $_POST["receipt_id"];
-    $account_name = $_POST["account_name"];
-    $support_email = $_POST["support_email"];
+$mailersend = new MailerSend(['api_key' => 'mlsn.8c0262e5612c1749b40a8d4119197e18855c2e442b044eafcc69326569876930']);
 
-    // Initialize MailerSend with your API key
-    $mailersend = new MailerSend(['api_key' => 'mlsn.a01bab1ddc19e7f8af9686193608e26729c39ca74e21ecd8a33d41d3d81e5789']);
+$recipients = [
+    new Recipient('recipient@example.com', 'Recipient Name'),
+];
 
-    // Create personalization and recipient objects
-    $personalization = [
-        new Personalization($support_email, [
-            'date' => $date,
-            'fees' => $fees,
-            'name' => $name,
-            'total' => $total,
-            'subtotal' => $subtotal,
-            'receipt_id' => $receipt_id,
-            'account_name' => $account_name,
-            'support_email' => $support_email
-        ])
-    ];
+$email = (new EmailParams())
+    ->setFrom('sender@example.com')
+    ->setFromName('Sender Name')
+    ->setRecipients($recipients)
+    ->setSubject('Test Email')
+    ->setHtml('<p>Hello world!</p>')
+    ->setText('Hello world!');
 
-    $recipients = [
-        new Recipient($support_email, 'Recipient Name'),
-    ];
-
-    // Build email parameters
-    $emailParams = (new EmailParams())
-        ->setFrom('your@domain.com')
-        ->setFromName('Your Name')
-        ->setRecipients($recipients)
-        ->setSubject('Subject')
-        ->setTemplateId('3zxk54v7wqq4jy6v')
-        ->setPersonalization($personalization);
-
-    // Send the email
-    $mailersend->email->send($emailParams);
-
-    // Optional: Redirect to a success page or display a success message
-    header("Location: success.html");
-    exit;
-}
+$mailerSend->email->send($email);
 ?>
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'f8109301663b5ec53ce1e179876bd3097361548f',
+        'reference' => '2f84114e213c472c58ed403e65c58569b0343c74',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'f8109301663b5ec53ce1e179876bd3097361548f',
+            'reference' => '2f84114e213c472c58ed403e65c58569b0343c74',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
