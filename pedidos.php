b67fcb04b62b5fdcde2480bfa0a74080abc1ddde
Sofia Donlucas Bañuelos <sof.db@outlook.com>
2023-08-03 18:15:07 -0600

    CourseCard and CourseInfo component

--- pedidos.php
+++ pedidos.php
@@ -1,22 +1,37 @@
 <?php
-use MailerSend\MailerSend;
-use MailerSend\Helpers\Builder\Personalization;
-use MailerSend\Helpers\Builder\Recipient;
-use MailerSend\Helpers\Builder\EmailParams;
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Obtener los datos del formulario
+    $date = $_POST["date"];
+    $fees = $_POST["fees"];
+    $name = $_POST["name"];
+    $total = $_POST["total"];
+    $subtotal = $_POST["subtotal"];
+    $receipt_id = $_POST["receipt_id"];
+    $account_name = $_POST["account_name"];
+    $support_email = $_POST["support_email"];
 
-$mailersend = new MailerSend(['api_key' => 'mlsn.8c0262e5612c1749b40a8d4119197e18855c2e442b044eafcc69326569876930']);
+    // Procesar los datos (por ejemplo, guardarlos en una base de datos)
+    // Aquí se asume que hay una conexión a la base de datos previamente establecida
+    $servername = "localhost";
+    $username = "tu_usuario";
+    $password = "tu_contraseña";
+    $dbname = "tu_basededatos";
 
-$recipients = [
-    new Recipient('recipient@example.com', 'Recipient Name'),
-];
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    if ($conn->connect_error) {
+        die("Error de conexión: " . $conn->connect_error);
+    }
 
-$email = (new EmailParams())
-    ->setFrom('sender@example.com')
-    ->setFromName('Sender Name')
-    ->setRecipients($recipients)
-    ->setSubject('Test Email')
-    ->setHtml('<p>Hello world!</p>')
-    ->setText('Hello world!');
+    $sql = "INSERT INTO pedidos (date, fees, name, total, subtotal, receipt_id, account_name, support_email) VALUES ('$date', '$fees', '$name', '$total', '$subtotal', '$receipt_id', '$account_name', '$support_email')";
 
-$mailerSend->email->send($email);
+    if ($conn->query($sql) === TRUE) {
+        echo "Pedido registrado exitosamente.";
+    } else {
+        echo "Error al registrar el pedido: " . $conn->error;
+    }
+
+    $conn->close();
+}
 ?>
+
